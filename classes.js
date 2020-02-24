e34726bab99db7b36fa99896fc9a576bc52d1734
Timothy Barna <audioclimb@gmail.com>
2020-02-24 12:29:56 -0700

    first commit

--- classes.js
+++ classes.js
@@ -29,7 +29,18 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+    makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
+const employeeOne = new Employee('Tim', 'Barna', 'email', 42)
 
 
 ////////// PROBLEM 2 //////////
